22c544927800ebb7861d0ba2555a718d1c27caaf
tky <takayuki.for.public@gmail.com>
2025-02-11 22:15:58 +0900

    Add Best Cow Line

---
+++ ch2-2/best-cow-line/src/main.rs
@@ -0,0 +1,62 @@
+fn main() {
+    let vs = "ACDBCB".chars().collect::<Vec<char>>();
+    let result = resolve(&vs);
+    println!("{}", result);
+}
+
+fn resolve(vs: &[char]) -> String {
+    let mut result = String::new();
+    let mut s = 0;
+    let mut e = vs.len() - 1;
+    while s <= e {
+        println!("s: {}, e: {}, {:?} {}", s, e, &vs[s..e], result);
+        if vs[s] < vs[e] {
+            result.push(vs[s]);
+            s += 1;
+        } else if vs[s] > vs[e] {
+            result.push(vs[e]);
+            e -= 1;
+        } else {
+            let mut rev = vs[s..e].to_vec();
+            rev.reverse();
+            if vs[s..e].to_vec() < rev {
+                result.push(vs[s]);
+                s += 1;
+            } else {
+                result.push(vs[e]);
+                if e == 0 {
+                    break;
+                }
+                e -= 1;
+            }
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_resolve() {
+        let test_cases = vec![
+            ("ACDBCB", "ABCBCD"),  // 基本ケース
+            ("ABCDE", "ABCDE"),    // すでに昇順
+            ("EDCBA", "ABCDE"),    // 逆順ケース
+            ("AABBA", "AAABB"),    // 部分的に同じ文字
+            ("ABABA", "AABAB"),    // 回文に近いケース
+            ("ZZZZZ", "ZZZZZ"),    // 同じ文字
+            ("A", "A"),            // 1文字ケース
+            ("BA", "AB"),          // 2文字逆順ケース
+            ("BBA", "ABB"),        // 3文字で同じ文字含み
+            ("XYZZYX", "XXYYZZ"),  // 対称なケース
+        ];
+
+        for (input, expected) in test_cases {
+            let vs = input.chars().collect::<Vec<char>>();
+            let result = resolve(&vs);
+            assert_eq!(result, expected, "Failed on input: {}", input);
+        }
+    }
+}
