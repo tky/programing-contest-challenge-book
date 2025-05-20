95493af7d3730cf3f91dda4427af6129918b3705
tky <takayuki.for.public@gmail.com>
2025-05-20 23:01:08 +0900

    Reduce unnecessary array

--- ch2-7/birde_the_prisoners/src/main.rs
+++ ch2-7/birde_the_prisoners/src/main.rs
@@ -7,14 +7,16 @@ fn solve(p: usize, a: &[usize]) -> usize {
     let mut v = [0].to_vec();
     v.extend(a);
     v.push(p + 1);
-    let mut dp = vec![vec![0; v.len() + 1]; v.len() + 1];
+    let n = v.len();
+    let mut dp = vec![vec![0; n]; n];
 
     for width in 2..v.len() {
         for i in 0..v.len() - width {
             let j = i + width;
+            let bribe = v[j] - v[i] - 2;
             dp[i][j] = usize::MAX;
             for k in i + 1..j {
-                dp[i][j] = dp[i][j].min(dp[i][k] + dp[k][j] + (v[j] - v[i] - 2));
+                dp[i][j] = dp[i][j].min(dp[i][k] + dp[k][j] + bribe);
             }
         }
     }
