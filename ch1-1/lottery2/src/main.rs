acc3582c83a3ccb76f56817d3db92aa7acb35e46
tky <takayuki.for.public@gmail.com>
2025-10-28 16:12:04 +0900

    Fix comment

--- ch1-1/lottery2/src/main.rs
+++ ch1-1/lottery2/src/main.rs
@@ -6,6 +6,10 @@ fn main() {
     solve3(3, 10, &vec![1, 3, 5]);
 }
 
+// kから4回選んで和がmになるかの判定を行う
+//
+// ka + kb + kc + kd = m であれば4回選んだ和がmになったことになる
+// この解放前パターンをチェックしており、計算量はO(n^4)である
 fn solve1(n: u32, m: u32, k: &Vec<u32>) {
     println!("n: {}, m: {}, k: {:?}", n, m, k);
     let mut f: bool = false;
@@ -13,9 +17,6 @@ fn solve1(n: u32, m: u32, k: &Vec<u32>) {
         for b in 0..n as usize {
             for c in 0..n as usize {
                 for d in 0..n as usize {
-                    // ka + kb + kc + kd = m
-                    // => 
-                    // kd = m - ka - kb - kc
                     if k[a] + k[b] + k[c] + k[d] == m {
                         f = true;
                     }
@@ -31,6 +32,12 @@ fn solve1(n: u32, m: u32, k: &Vec<u32>) {
     }
 }
 
+// 改良版
+// ka + kb + kc + kd = m
+// より、kd = m - (ka + kb + kc) と変形できる
+// つまり配列kにkdが存在するかを二分探索で調べればよい
+// 配列の探索は２分探索を用いることで計算量はO(n^3 log n)に削減できる
+// kは事前にソートしておく必要がある
 fn solve2(n: u32, m: u32, k: &Vec<u32>) {
     println!("n: {}, m: {}, k: {:?}", n, m, k);
     let mut f: bool = false;
@@ -55,14 +62,25 @@ fn solve2(n: u32, m: u32, k: &Vec<u32>) {
     }
 }
 
+// さらに改良版
+// ka + kb + kc + kd = m
+// より、(ka + kb) + (kc + kd) = m と変形できる
+// (ka + kb)の組み合わせを事前に計算しておき、(kc + kd) = m - (ka + kb)が存在するかを二分探索で調べればよい
+// (ka + kb)の組み合わせはn^2通りあるので、事前計算にO(n^2)かかる
+// 計算量はO(n^2 log n)に削減できる
 fn solve3(n: u32, m: u32, k: &Vec<u32>) {
     println!("n: {}, m: {}, k: {:?}", n, m, k);
-    let mut kk = k.iter().combinations(2).map(|v| {
-        v.iter().fold(0, |acc, &x| acc + x)
-    }).collect_vec();
+
+    // (ka + kb)の組み合わせを事前計算してsortしておく
+    let mut kk = k
+        .iter()
+        .combinations(2)
+        .map(|vs| vs.iter().copied().sum::<u32>())
+        .collect_vec();
     kk.sort();
 
     let mut f = false;
+    // (kc + kd) = m - (ka + kb)が存在するかを二分探索で調べる
     for a in 0..n as usize {
         for b in 0..n as usize {
             let sum = k[a] + k[b];
@@ -86,19 +104,19 @@ fn binary_search(x: u32, k: &Vec<u32>) -> bool {
     while r - l >= 1 {
         let i = (l + r) / 2;
         if k[i] == x {
-            return true
+            return true;
         } else if k[i] < x {
             l = i + 1;
         } else {
             r = i;
         }
     }
-    return false
+    return false;
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{binary_search};
+    use super::binary_search;
     #[test]
     fn test_binary_search_with_odd_input() {
         let k = vec![1, 3, 5];
