7e1a5e44ca3df44aab17e989f05c847407d45cd3
tky <takayuki.for.public@gmail.com>
2025-10-29 20:53:12 +0900

    format and add comment

--- ch2-2/scheduling/src/main.rs
+++ ch2-2/scheduling/src/main.rs
@@ -1,16 +1,36 @@
 struct Task {
-    id: &'static str,
+    id: String,
     start: u32,
     end: u32,
 }
 
 fn main() {
     let tasks = vec![
-        Task { id: "1", start: 1, end: 3 },
-        Task { id: "2", start: 2, end: 5 },
-        Task { id: "3", start: 4, end: 7 },
-        Task { id: "4", start: 6, end: 9 },
-        Task { id: "5", start: 8, end: 10 },
+        Task {
+            id: "1".to_string(),
+            start: 1,
+            end: 3,
+        },
+        Task {
+            id: "2".to_string(),
+            start: 2,
+            end: 5,
+        },
+        Task {
+            id: "3".to_string(),
+            start: 4,
+            end: 7,
+        },
+        Task {
+            id: "4".to_string(),
+            start: 6,
+            end: 9,
+        },
+        Task {
+            id: "5".to_string(),
+            start: 8,
+            end: 10,
+        },
     ];
     let results = solve(&tasks);
     let job_str = results.iter().map(|task| task.id.to_string());
@@ -30,5 +50,10 @@ fn solve(tasks: &[Task]) -> Vec<&Task> {
 
 // find the next task that can be executed and which ends the earliest
 fn find_next_task(tasks: &[Task], current_time: u32) -> Option<&Task> {
-    tasks.iter().filter(|task| task.start > current_time).min_by_key(|task| task.end)
+    tasks
+        .iter()
+        // 現在時刻より後に開始できるタスクから
+        .filter(|task| task.start > current_time)
+        // もっと思早く終わるタスクを選択
+        .min_by_key(|task| task.end)
 }
