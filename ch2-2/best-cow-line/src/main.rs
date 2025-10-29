be244541d8a179bba8a1254a7ec975de7ab7d9cd
tky <takayuki.for.public@gmail.com>
2025-10-29 21:19:33 +0900

    add comment

--- ch2-2/best-cow-line/src/main.rs
+++ ch2-2/best-cow-line/src/main.rs
@@ -9,16 +9,20 @@ fn resolve(vs: &[char]) -> String {
     let mut s = 0;
     let mut e = vs.len() - 1;
     while s <= e {
-        println!("s: {}, e: {}, {:?} {}", s, e, &vs[s..e], result);
+        println!("s: {}, e: {}, {:?} {}", s, e, &vs[s..=e], result);
+        // 先頭の方が早い場合は先頭を先頭を追加
         if vs[s] < vs[e] {
             result.push(vs[s]);
             s += 1;
+        // 末尾の方が早い場合は末尾を追加
         } else if vs[s] > vs[e] {
             result.push(vs[e]);
             e -= 1;
         } else {
+            // 同じ場合は反転した文字列を比較
             let mut rev = vs[s..e].to_vec();
             rev.reverse();
+            // 元の方が早ければ先頭を追加、そうでなければ末尾を追加
             if vs[s..e].to_vec() < rev {
                 result.push(vs[s]);
                 s += 1;
@@ -41,16 +45,16 @@ mod tests {
     #[test]
     fn test_resolve() {
         let test_cases = vec![
-            ("ACDBCB", "ABCBCD"),  // 基本ケース
-            ("ABCDE", "ABCDE"),    // すでに昇順
-            ("EDCBA", "ABCDE"),    // 逆順ケース
-            ("AABBA", "AAABB"),    // 部分的に同じ文字
-            ("ABABA", "AABAB"),    // 回文に近いケース
-            ("ZZZZZ", "ZZZZZ"),    // 同じ文字
-            ("A", "A"),            // 1文字ケース
-            ("BA", "AB"),          // 2文字逆順ケース
-            ("BBA", "ABB"),        // 3文字で同じ文字含み
-            ("XYZZYX", "XXYYZZ"),  // 対称なケース
+            ("ACDBCB", "ABCBCD"), // 基本ケース
+            ("ABCDE", "ABCDE"),   // すでに昇順
+            ("EDCBA", "ABCDE"),   // 逆順ケース
+            ("AABBA", "AAABB"),   // 部分的に同じ文字
+            ("ABABA", "AABAB"),   // 回文に近いケース
+            ("ZZZZZ", "ZZZZZ"),   // 同じ文字
+            ("A", "A"),           // 1文字ケース
+            ("BA", "AB"),         // 2文字逆順ケース
+            ("BBA", "ABB"),       // 3文字で同じ文字含み
+            ("XYZZYX", "XXYYZZ"), // 対称なケース
         ];
 
         for (input, expected) in test_cases {
