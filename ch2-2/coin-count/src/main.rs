74325d600f1c4091be7b2ef8e118824e749b22f7
tky <takayuki.for.public@gmail.com>
2025-10-29 20:42:03 +0900

    format and fix typo

--- ch2-2/coin-count/src/main.rs
+++ ch2-2/coin-count/src/main.rs
@@ -5,16 +5,31 @@ fn main() {
     println!("{}", solve(c1, c5, c10, c50, c100, c500, a));
 }
 
-fn solve(c1: usize, c5: usize, c10: usize, c50: usize, c100: usize, c500: usize, a: usize) -> usize {
+fn solve(
+    c1: usize,
+    c5: usize,
+    c10: usize,
+    c50: usize,
+    c100: usize,
+    c500: usize,
+    a: usize,
+) -> usize {
     let mut ans = 0;
     let mut remain = a;
 
-    for (numer_of_coin, price) in [(c500, 500), (c100, 100), (c50, 50), (c10, 10), (c5, 5), (c1, 1)] {
+    for (number_of_coin, price) in [
+        (c500, 500),
+        (c100, 100),
+        (c50, 50),
+        (c10, 10),
+        (c5, 5),
+        (c1, 1),
+    ] {
         if remain == 0 {
             break;
         }
         if remain >= price {
-            let paid_count = cmp::min(numer_of_coin, remain / price);
+            let paid_count = cmp::min(number_of_coin, remain / price);
             let paid_amount = paid_count * price;
 
             ans += paid_count;
@@ -26,7 +41,7 @@ fn solve(c1: usize, c5: usize, c10: usize, c50: usize, c100: usize, c500: usize,
 
 #[cfg(test)]
 mod test {
-    use super::{solve};
+    use super::solve;
 
     #[test]
     fn test_solve() {
